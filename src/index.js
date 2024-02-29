b58309027c74fc2155af295f0eba3a67132ee72a
ysyapr91 <ysyapr91@gmail.com>
2024-02-29 13:58:36 +0900

    .

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store/store.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
